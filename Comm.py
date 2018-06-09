fa5df3c48dea1a955de00dc037bcc02733fad20a
chanlo <woluch@foxmail.com>
2018-06-09 23:54:08 +0800

    'create.'

---
+++ Comm.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+class Comm(object):
+    def __init__(self):
+        pass
+    
+    def Knock(self):
+        pass
+
+if __name__ == '__main__':
+    pass
---
+++ Sensor.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+class Sensor(object):
+    def __init__(self):
+        pass
+    
+    def PhotoSensor(self):
+        pass
+    
+    def CurrentTrigger(self):
+        pass
+
+    def ADSample(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
