80940053af13c5c579002b60dbbafdb5fa5f5a11
chanlo <woluch@foxmail.com>
2018-06-17 16:22:17 +0800

    'update'

--- DCMotor.py
+++ DCMotor.py
@@ -1,12 +1,36 @@
 #coding:utf-8
+import RPi.GPIO as GPIO
 
-class DCMotor(object):
+class DCMotor(object, PWMPin, dirPin1, dirPin2, PWMFreq):
     def __init__(self):
-        pass
-    
-    def DCRun(self):
-        pass
-    
+        self.PWMPin = PWMPin
+        self.dirPin1 = dirPin1
+        self.dirPin2 = dirPin2
+        self.PWMFreq = PWMFreq
+        self.DCMotorInit
 
+    def DCMotorInit(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.PWMPin, GPIO.OUT)
+        GPIO.setup(self.dirPin1, GPIO.OUT)
+        GPIO.setup(self.dirPin2, GPIO.OUT)
+        self.PWM = GPIO.PWM(self.PWMPin, self.PWMFreq)
+        GPIO.output(self.dirPin1, True)
+        GPIO.output(self.dirPin2, False)
+
+    def MotorForward(self):
+        GPIO.output(self.dirPin1, True)
+        GPIO.output(self.dirPin2, False)
+
+    def MoterReverse(self):
+        GPIO.output(self.dirPin1, False)
+        GPIO.output(self.dirPin2, True)
+    
+    def SpeedSet(self, speed):
+        self.PWM.ChangeDutyCycle(speed)
+    
+    def MoterStart(self):
+        self.PWM.start(90)    # 以输出占空比90%开始
+    
 if __name__ == '__main__':
     pass
--- Steer.py
+++ Steer.py
@@ -1,11 +1,25 @@
 #coding:utf-8
+import RPi.GPIO as GPIO
+import signal
+import atexit
 
-class Sensor(object):
+class Steer(object, servoPin):
     def __init__(self):
-        pass
+        self.servoPin = servoPin
+        self.SteerInit
+
+    def SteerInit(self):
+        atexit.register(GPIO.cleanup)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.set(self.servoPin, GPIO.OUT, initial=False)
+        self.p = GPIO.PWM(self.servoPin, 50)
+        self.p.start(5)
     
-    def SteerRun(self):
-        pass
+    def SteerForward(self):
+        self.p.ChangeDutyCycle(10)
+
+    def SteerReverse(self):
+        self.p.ChangeDutyCycle(5)
     
 
 if __name__ == '__main__':
